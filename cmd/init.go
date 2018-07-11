8d7553de9bfb095f1b0064e8c5ed4d39bef88dd9
Sridhar <s@vaishnavy.com>
2018-07-11 19:10:07 +0200

    Walking through the files and printing them for the moment

--- cmd/init.go
+++ cmd/init.go
@@ -2,21 +2,23 @@ package cmd
 
 import (
   "fmt"
-
   "github.com/spf13/cobra"
   "github.com/gobuffalo/packr"
 )
 
 func init() {
-  rootCmd.AddCommand(versionCmd)
+  rootCmd.AddCommand(initCmd)
 }
 
-var versionCmd = &cobra.Command{
+var initCmd = &cobra.Command{
   Use:   "init AwesomeProjectName [flags]",
   Short: "Initialize your awesome cosmos-sdk zone", 
   Run: func(cmd *cobra.Command, args []string) {
     box := packr.NewBox("../template")
-    gopkg := box.String("Gopkg.toml")
+    gopkg := box.Walk(func(path string, file packr.File) error { 
+        fmt.Println(path)
+        return nil
+    })
     fmt.Println(gopkg)
     fmt.Println("Initialized a new project, happy hacking!")
   },
