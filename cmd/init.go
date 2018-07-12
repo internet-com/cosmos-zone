ac8a7d8cbb7f01eae7b78b6fe70b66fb47399f0d
Sridhar <s@vaishnavy.com>
2018-07-12 03:36:25 +0200

    Changing the short description of the commands

--- cmd/init.go
+++ cmd/init.go
@@ -1,25 +1,26 @@
 package cmd
 
 import (
-  "fmt"
-  "github.com/spf13/cobra"
-  "github.com/gobuffalo/packr"
+	"fmt"
+
+	"github.com/gobuffalo/packr"
+	"github.com/spf13/cobra"
 )
 
 func init() {
-  rootCmd.AddCommand(initCmd)
+	rootCmd.AddCommand(initCmd)
 }
 
 var initCmd = &cobra.Command{
-  Use:   "init AwesomeProjectName [flags]",
-  Short: "Initialize your awesome cosmos-sdk zone", 
-  Run: func(cmd *cobra.Command, args []string) {
-    box := packr.NewBox("../template")
-    gopkg := box.Walk(func(path string, file packr.File) error { 
-        fmt.Println(path)
-        return nil
-    })
-    fmt.Println(gopkg)
-    fmt.Println("Initialized a new project, happy hacking!")
-  },
+	Use:   "init AwesomeProjectName",
+	Short: "Initialize your new cosmos zone",
+	Run: func(cmd *cobra.Command, args []string) {
+		box := packr.NewBox("../template")
+		gopkg := box.Walk(func(path string, file packr.File) error {
+			fmt.Println(path)
+			return nil
+		})
+		fmt.Println(gopkg)
+		fmt.Println("Initialized a new project, happy hacking!")
+	},
 }
